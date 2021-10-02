243a8d5f0434dcde6e3ae76398b1f75e30874427
DroidDude <freetime9630@gmail.com>
2021-10-02 15:20:54 +0200

    Initial commit

---
+++ src/main/java/eu/newaustrianservers/fltpot/block/StrangeDustOre.java
@@ -0,0 +1,23 @@
+package eu.newaustrianservers.fltpot.block;
+
+import net.minecraft.block.OreBlock;
+import net.minecraft.util.math.MathHelper;
+
+import java.util.Random;
+
+public class StrangeDustOre extends OreBlock {
+
+    public StrangeDustOre(Properties properties) {
+
+        super(properties);
+
+    }
+
+    @Override
+    protected int xpOnDrop(Random p_220281_1_) {
+
+        return MathHelper.nextInt(p_220281_1_, 3, 7);
+
+    }
+
+}
