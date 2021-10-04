ab62074c0940f8ab4d0dc27369784b4b3ec613d5
DroidDude <freetime9630@gmail.com>
2021-10-04 18:26:46 +0200

    Initial commit

--- src/main/java/eu/newaustrianservers/fltpot/world/OreGeneration.java
+++ src/main/java/eu/newaustrianservers/fltpot/world/OreGeneration.java
@@ -2,15 +2,10 @@ package eu.newaustrianservers.fltpot.world;
 
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod.EventBusSubscriber
 public class OreGeneration {
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void gen() {
         for (Biome biome : ForgeRegistries.BIOMES) {
             biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, OreFeatures.FEATURE_STRANGE_DUST_ORE);
