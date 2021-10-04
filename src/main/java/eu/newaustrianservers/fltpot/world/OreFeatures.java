495beee565c2a857caa7eb249a50621b9f5981d9
DroidDude <freetime9630@gmail.com>
2021-10-04 18:32:57 +0200

    Initial commit

--- src/main/java/eu/newaustrianservers/fltpot/world/OreFeatures.java
+++ src/main/java/eu/newaustrianservers/fltpot/world/OreFeatures.java
@@ -9,6 +9,6 @@ import net.minecraft.world.gen.placement.Placement;
 
 public class OreFeatures {
 
-    public static final ConfiguredFeature<?, ?> FEATURE_STRANGE_DUST_ORE = Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.STRANGE_DUST_ORE.get().getDefaultState(), 5)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(2, 1, 0, 10)));
+    public static final ConfiguredFeature<?, ?> FEATURE_STRANGE_DUST_ORE = Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.STRANGE_DUST_ORE.get().getDefaultState(), 3)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(2, 1, 0, 10)));
 
 }
