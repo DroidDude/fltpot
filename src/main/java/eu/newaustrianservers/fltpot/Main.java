2f8c453b8fda2243c8def8a34514782e09fcce51
DroidDude <freetime9630@gmail.com>
2021-10-25 21:22:17 +0200

    0.2.3

--- src/main/java/eu/newaustrianservers/fltpot/Main.java
+++ src/main/java/eu/newaustrianservers/fltpot/Main.java
@@ -4,19 +4,6 @@ import eu.newaustrianservers.fltpot.block.Blocks;
 import eu.newaustrianservers.fltpot.effect.Effects;
 import eu.newaustrianservers.fltpot.item.Items;
 import eu.newaustrianservers.fltpot.potion.Potions;
-import eu.newaustrianservers.fltpot.world.OreFeatures;
-import net.minecraft.data.worldgen.SurfaceBuilders;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeGenerationSettings;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.OreFeature;
-import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -50,4 +37,5 @@ public class Main{
         LOGGER.info("Setup method registered.");
 
     }
+
 }
--- src/main/java/eu/newaustrianservers/fltpot/potion/Potions.java
+++ src/main/java/eu/newaustrianservers/fltpot/potion/Potions.java
@@ -3,6 +3,7 @@ package eu.newaustrianservers.fltpot.potion;
 import eu.newaustrianservers.fltpot.Main;
 import eu.newaustrianservers.fltpot.effect.Effects;
 import eu.newaustrianservers.fltpot.item.Items;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
@@ -23,12 +24,14 @@ public class Potions extends net.minecraft.world.item.alchemy.Potions {
 
     public static final RegistryObject<Potion> FLIGHT_POTION = POTIONS.register("flight_potion", () -> new Potion( new MobEffectInstance(Effects.FLIGHT.get(), 2400)));
     public static final RegistryObject<Potion> LONG_FLIGHT_POTION = POTIONS.register("long_flight_potion", () -> new Potion( new MobEffectInstance(Effects.FLIGHT.get(), 9600)));
+    public static final RegistryObject<Potion> LEVITATION_POTION = POTIONS.register("levitation", () -> new Potion( new MobEffectInstance(Effects.LEVITATION, 200)));
 
 
     public static void addBrewingRecipe() {
 
         BrewingRecipeRegistry.addRecipe(new FlightPotion(null, null, null));
         BrewingRecipeRegistry.addRecipe(new LongFlightPotion(null, null, null));
+        BrewingRecipeRegistry.addRecipe(new LevitationPotion(null, null, null));
 
     }
 
@@ -82,6 +85,33 @@ public class Potions extends net.minecraft.world.item.alchemy.Potions {
         public ItemStack getOutput(@Nonnull ItemStack input, @Nonnull ItemStack ingredient)
         {
             if (isInput(input) && isIngredient(ingredient)) return PotionUtils.setPotion(new ItemStack(input.getItem()), LONG_FLIGHT_POTION.get());
+            return ItemStack.EMPTY;
+        }
+    }
+
+
+    public static class LevitationPotion extends BrewingRecipe {
+
+        private static Ingredient INPUT = Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.FLIGHT_POTION.get()));
+        private static Ingredient REAGENT = Ingredient.of(Items.FERMENTED_SPIDER_EYE);
+        private static ItemStack OUTPUT = PotionUtils.setPotion(new ItemStack(Items.POTION), LEVITATION_POTION.get());
+
+        public LevitationPotion(@Nonnull Ingredient start, @Nonnull Ingredient input, @Nonnull ItemStack output) {
+
+            super(INPUT, REAGENT, OUTPUT);
+        }
+
+        @Override
+        public boolean isInput(@Nonnull ItemStack stack) {
+
+            return PotionUtils.getPotion(stack) == Potions.FLIGHT_POTION.get();
+        }
+
+        @Nonnull
+        @Override
+        public ItemStack getOutput(@Nonnull ItemStack input, @Nonnull ItemStack ingredient)
+        {
+            if (isInput(input) && isIngredient(ingredient)) return PotionUtils.setPotion(new ItemStack(input.getItem()), LEVITATION_POTION.get());
             return ItemStack.EMPTY;
         }
     }
