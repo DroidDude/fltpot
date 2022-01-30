28c4535872d7c5a78ae35f19c38eeb475a941d45
sneusse <s.neusser@gmail.com>
2022-01-30 18:08:01 +0100

    - removed client access - autoformatted file

--- src/main/java/eu/newaustrianservers/fltpot/event/Events.java
+++ src/main/java/eu/newaustrianservers/fltpot/event/Events.java
@@ -1,7 +1,6 @@
 package eu.newaustrianservers.fltpot.event;
 
 import eu.newaustrianservers.fltpot.effect.Effects;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
@@ -19,17 +18,17 @@ import net.minecraftforge.fml.common.Mod;
 public class Events {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void doFlying(TickEvent.PlayerTickEvent event){
+    public static void doFlying(TickEvent.PlayerTickEvent event) {
 
         PlayerEntity player = event.player;
         CompoundNBT tag = player.getPersistentData();
         boolean wasFlying = tag.getBoolean("wasFlying");
 
-        if(!player.isCreative() && !player.isSpectator()){
+        if (!player.isCreative() && !player.isSpectator()) {
 
-            if(player.hasEffect(Effects.FLIGHT.get()) && !player.hasEffect(Effects.LEVITATION)){
+            if (player.hasEffect(Effects.FLIGHT.get()) && !player.hasEffect(Effects.LEVITATION)) {
 
-                if(!player.abilities.mayfly){
+                if (!player.abilities.mayfly) {
 
                     player.abilities.mayfly = true;
                     tag.putBoolean("wasFlying", true);
@@ -37,19 +36,19 @@ public class Events {
 
                 }
 
-                if(!player.isOnGround() && player.abilities.flying && player.getY() <= player.yOld && !Minecraft.getInstance().options.keyJump.isDown()){
+                if (!player.isOnGround() && player.abilities.flying && player.getY() <= player.yOld) {
 
                     Vector3d vec = new Vector3d(0d, -0.1d, 0d);
                     player.move(MoverType.PLAYER, vec);
 
-                } else if(player.isOnGround()){
+                } else if (player.isOnGround()) {
 
                     player.abilities.flying = false;
                     player.onUpdateAbilities();
 
                 }
 
-            } else if(wasFlying && !player.hasEffect(Effects.FLIGHT.get()) || player.hasEffect(Effects.LEVITATION)){
+            } else if (wasFlying && !player.hasEffect(Effects.FLIGHT.get()) || player.hasEffect(Effects.LEVITATION)) {
 
                 player.abilities.mayfly = false;
                 player.abilities.flying = false;
@@ -63,13 +62,13 @@ public class Events {
     }
 
     @SubscribeEvent
-    public static void addNBTData(PlayerEvent.PlayerLoggedInEvent event){
+    public static void addNBTData(PlayerEvent.PlayerLoggedInEvent event) {
 
         PlayerEntity player = event.getPlayer();
         CompoundNBT tag = player.getPersistentData();
         INBT modeTag = tag.get("wasFlying");
 
-        if(modeTag == null){
+        if (modeTag == null) {
 
             tag.putBoolean("wasFlying", false);
 
@@ -83,9 +82,9 @@ public class Events {
         double distance = event.getDistance();
         PlayerEntity player = event.getPlayer();
 
-        if (distance >= 3 && !player.isCreative() && player.hasEffect(Effects.FLIGHT.get())){
+        if (distance >= 3 && !player.isCreative() && player.hasEffect(Effects.FLIGHT.get())) {
 
-            float damage = (float)Math.floor(distance) - 2;
+            float damage = (float) Math.floor(distance) - 2;
             player.hurt(DamageSource.FALL, damage);
 
         }
